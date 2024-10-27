b7a2090ac0881fbff43017ad36357286a818d94b
Esmeail Mashhadi <codealchemy.esi@gmail.com>
2024-10-27 18:45:00 +0330

    unenved

--- app/config/connectdb.js
+++ app/config/connectdb.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.mongoURI);
+    await mongoose.connect('mongodb+srv://Alchemist_man:esi2022esi@cluster1.pueofkb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1');
     console.log('Connected to the database');
   } catch (error) {
     console.error('Failed to connect to the database:', error);
